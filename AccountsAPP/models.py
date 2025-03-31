a0e9d24d67c2fbc8b05f93a5dfb1808a28519ab0
forestsol <forestsol1214@gmail.com>
2025-03-31 21:20:30 +0900

    feat: update quest model
    feat: update quest model

--- AccountsAPP/models.py
+++ AccountsAPP/models.py
@@ -11,6 +11,7 @@ class CustomUser(AbstractUser):
 class Quest(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    useCamera = models.BooleanField(default=False)
 
 
 # 사용자 퀘스트 인증
