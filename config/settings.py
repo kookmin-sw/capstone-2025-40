1c95f33822b4244b990f88a5bd1dcb71fdef5f13
forestsol <forestsol1214@gmail.com>
2025-05-18 20:53:07 +0900

    Revert "Merge branch 'production' into back"
    This reverts commit 37fe04c6d6dd57ed0de6946df7123fb86870204b, reversing
    changes made to 075c3bade5ea0ed4f151f50432a0e9a49541ed38.

--- config/settings.py
+++ config/settings.py
@@ -142,12 +142,9 @@ INSTALLED_APPS = [
     'drf_yasg',
     'AccountsAPP',
     'django_extensions'
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -157,14 +154,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-#CORS_ALLOWED_ORIGINS = [
-#    "http://localhost:3000",
-#    "http://greenday-8d0a5.web.app"
-#]
-
 ROOT_URLCONF = 'config.urls'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
